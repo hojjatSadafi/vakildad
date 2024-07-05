a6241427704953c855dac36b4e8200f4c369115a
hojjat sadafi <161528589+hojjatSadafi@users.noreply.github.com>
2024-07-05 12:04:58 +0430

    create new login structure

---
+++ js/newpanel.js
@@ -0,0 +1,46 @@
+// let $ = document
+// let get = function (id){return document.getElementById(id)}
+// let images=[
+//     "images/judgeicons/1.png",
+//     "images/judgeicons/2.png",
+//     "images/judgeicons/3.png",
+//     "images/judgeicons/4.png",
+//     "images/judgeicons/5.png",
+// ]
+
+// const arrowrightBtn = get('right-arrow')
+// const arrowLeftBtn = get('left-arrow')
+// let imageIndex = 0
+
+// let imageElem = get('temlate-left__img')
+
+// arrowLeftBtn.addEventListener('click',pervImage)
+// arrowrightBtn.addEventListener('click',nextImage)
+
+// function nextImage(){
+//     imageIndex++
+//     if(imageIndex === images.length){
+//         imageIndex = 0
+//     }
+//     imageElem.src = images[imageIndex]
+//     addAnim()
+// }
+// function pervImage(){
+//     imageIndex--
+//     if(imageIndex < 0){
+//         imageIndex = images.length-1
+//     }
+//     imageElem.src = images[imageIndex]
+//     addAnim()
+// }
+
+// function addAnim(){
+//     imageElem.style.animation = "imageAnim 500ms 1 both "
+//     setTimeout(() => {
+//         imageElem.style.animation = ""
+//     }, 500);
+// }
+
+// setInterval(function(){
+//     nextImage()
+// },7000)
